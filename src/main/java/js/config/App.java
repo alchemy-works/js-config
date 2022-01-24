6286f4a8011df77ced248f69fc4d7e3c1495afff
cbdyzj <cbdyzj@jianzhao.org>
2022-01-24 23:45:59 +0800

    Use Data URL

--- src/main/java/js/config/App.java
+++ src/main/java/js/config/App.java
@@ -4,16 +4,24 @@ import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Map;
 
 public class App {
 
     public Map<?, ?> config() throws IOException {
-        var script = "import config from './config.js';config";
-        return Context.newBuilder("js")
-                .allowIO(true)
-                .build()
-                .eval(Source.newBuilder("js", script, "_.mjs").build())
-                .as(Map.class);
+        var s = """
+                export default {
+                    name: 'JavaScript Configuration'
+                }
+                """;
+
+        var script = "(await import('data:text/javascript;base64,%s'))['default']".formatted(Base64.getEncoder().encodeToString(s.getBytes(StandardCharsets.UTF_8)));
+        var context = Context.newBuilder("js").build();
+        try (context) {
+            var source = Source.newBuilder("js", script, "_.mjs").build();
+            return context.eval(source).as(Map.class);
+        }
     }
 }
